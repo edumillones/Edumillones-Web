19ec2bd6a28e758ea4d2e6de32a6c930765836f4
Eduardo Millones <119453622+edumillones@users.noreply.github.com>
2024-02-17 12:16:57 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,6 @@ export default function Home() {
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
-        <PackageFeatures />
         <EmailSection />
       </div>
       <Footer />
