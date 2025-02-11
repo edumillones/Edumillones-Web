3fd0bdf511457f1ea2730592e151f3162eb12881
Eduardo Millones <119453622+edumillones@users.noreply.github.com>
2025-02-11 11:20:42 -0500

    Update and rename spotify.ts to spotify-auth.ts

---
+++ src/app/lib/spotify-auth.ts
@@ -0,0 +1,24 @@
+export const getAccessToken = async () => {
+  const client_id = process.env.SPOTIFY_CLIENT_ID;
+  const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+  const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
+
+  if (!client_id || !client_secret || !refresh_token) {
+    throw new Error('Missing Spotify environment variables');
+  }
+
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${Buffer.from(`${client_id}:${client_secret}`).toString('base64')}`,
+    },
+    body: new URLSearchParams({
+      grant_type: 'refresh_token',
+      refresh_token,
+    }),
+  });
+
+  const data = await response.json();
+  return data.access_token;
+};
